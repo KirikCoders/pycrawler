d6e2cfb69a8d0eac22ec0183e434263254995e01
datron <kartik1702@gmail.com>
2017-10-15 21:39:25 +0530

    Output file now generated for each try

--- crawler.py
+++ crawler.py
@@ -1,22 +1,31 @@
-import requests,bs4
+import requests,bs4,os
 
 
-def get_vtu_page(url, usn):
+def get_vtu_page(file,url, usn):
     form_data = {'usn': usn}
     req = requests.post(url, data=form_data)
     soup = bs4.BeautifulSoup(req.text, "html.parser")
     tags = soup.select('td')
-    print(tags[1].getText()+"\n")
     n = len(tags)
-    print(n)
+    file.write(tags[1].getText() + "\n")
     for i in range(4, n-6, 6):
         str = tags[i].getText()+" "+tags[i+1].getText()+" "+tags[i+2].getText()+" "+tags[i+3].getText()+" "+tags[i+4].getText()+" "+tags[i+5].getText()
-        print(str+"\n")
+        file.write(str+"\n\n\n")
 
 
 url = "http://results.vtu.ac.in/cbcs_17/result_page.php"
 start_seq = "1BI15CS"
+pwd = os.getcwd()
+path = os.path.join(pwd, 'tests')
+if not os.path.exists(path):
+    os.makedirs(path)
+write_path = os.path.join(path, 'out.txt')
+if os.path.exists(write_path):
+    os.remove(write_path)
+file1 = open(write_path, 'a')
+print("working", end='', flush=True)
 for i in range(200):
+    print('.', end='', flush=True)
     if len(str(i)) == 1:
         roll = "00"+str(i)
     elif len(str(i)) == 2:
@@ -24,4 +33,6 @@ for i in range(200):
     else:
         roll = str(i)
     usn = start_seq + roll
-    get_vtu_page(url, usn)
+    get_vtu_page(file1, url, usn)
+print("\ndone.")
+file1.close()
