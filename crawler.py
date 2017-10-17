e8f405400fa871d61fc914a1c682573a2924536d
datron <kartik1702@gmail.com>
2017-10-17 21:40:27 +0530

    added excel capabilities and crawler now works for all cases

--- crawler.py
+++ crawler.py
@@ -1,16 +1,46 @@
-import requests,bs4,os
+import requests
+import bs4
+import os
+import openpyxl
 
 
-def get_vtu_page(file,url, usn):
+def get_vtu_page(sheet, url, usn, row_no):
     form_data = {'usn': usn}
     req = requests.post(url, data=form_data)
     soup = bs4.BeautifulSoup(req.text, "html.parser")
     tags = soup.select('td')
     n = len(tags)
-    file.write(tags[1].getText() + "\n")
-    for i in range(4, n-6, 6):
-        str = tags[i].getText()+" "+tags[i+1].getText()+" "+tags[i+2].getText()+" "+tags[i+3].getText()+" "+tags[i+4].getText()+" "+tags[i+5].getText()
-        file.write(str+"\n\n\n")
+    if tags[4].getText() != "15MAT41":
+        return None
+    try:
+        sheet['A1'] = 'USN'
+        sheet['B1'] = 'Name'
+        # print(tags[4].getText())
+        sheet['C1'] = tags[4].getText()
+        sheet['D1'] = tags[10].getText()
+        sheet['E1'] = tags[16].getText()
+        sheet['F1'] = tags[22].getText()
+        sheet['G1'] = tags[28].getText()
+        sheet['H1'] = tags[34].getText()
+        sheet['I1'] = tags[40].getText()
+        sheet['J1'] = tags[46].getText()
+    except IndexError:
+        return None
+    cell = str(row_no)
+    usn = tags[1].getText()
+    name = tags[3].getText()
+    sheet['A'+cell] = usn[3:]
+    sheet['B'+cell] = name[1:]
+    col = 'C'
+    for i in range(8, n-6, 6):
+        sheet[col+cell] = tags[i].getText()
+        col = get_next_cell(col)
+        if col == 'K':
+            break
+
+
+def get_next_cell(current_cell):
+    return chr(ord(current_cell)+1)
 
 
 url = "http://results.vtu.ac.in/cbcs_17/result_page.php"
@@ -19,12 +49,11 @@ pwd = os.getcwd()
 path = os.path.join(pwd, 'tests')
 if not os.path.exists(path):
     os.makedirs(path)
-write_path = os.path.join(path, 'out.txt')
-if os.path.exists(write_path):
-    os.remove(write_path)
-file1 = open(write_path, 'a')
+wb = openpyxl.Workbook()
+sheet = wb.get_active_sheet()
+sheet.title = "5th Sem"
 print("working", end='', flush=True)
-for i in range(200):
+for i in range(1, 200):
     print('.', end='', flush=True)
     if len(str(i)) == 1:
         roll = "00"+str(i)
@@ -33,6 +62,6 @@ for i in range(200):
     else:
         roll = str(i)
     usn = start_seq + roll
-    get_vtu_page(file1, url, usn)
+    get_vtu_page(sheet, url, usn, i+1)
 print("\ndone.")
-file1.close()
+wb.save("output.xlsx")
