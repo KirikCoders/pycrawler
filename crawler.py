383d5c5defd39dce0ee2931a8ff62ffa489c0c9e
datron <kartik1702@gmail.com>
2017-10-27 16:37:12 +0530

    gets 14 series as well

--- crawler.py
+++ crawler.py
@@ -4,26 +4,24 @@ import os
 import openpyxl
 
 
-def get_vtu_page(sheet, url, usn, row_no):
+def get_vtu_page(sheet, url, usn, row_no, filter_sub):
     form_data = {'usn': usn}
     req = requests.post(url, data=form_data)
     soup = bs4.BeautifulSoup(req.text, "html.parser")
     tags = soup.select('td')
     n = len(tags)
-    if tags[4].getText() != "15MAT41":
+    if tags[4].getText() != filter_sub:
         return None
     try:
-        sheet['A1'] = 'USN'
-        sheet['B1'] = 'Name'
         # print(tags[4].getText())
         sheet['C1'] = tags[4].getText()
-        sheet['D1'] = tags[10].getText()
-        sheet['E1'] = tags[16].getText()
-        sheet['F1'] = tags[22].getText()
-        sheet['G1'] = tags[28].getText()
-        sheet['H1'] = tags[34].getText()
-        sheet['I1'] = tags[40].getText()
-        sheet['J1'] = tags[46].getText()
+        sheet['F1'] = tags[10].getText()
+        sheet['I1'] = tags[16].getText()
+        sheet['L1'] = tags[22].getText()
+        sheet['O1'] = tags[28].getText()
+        sheet['R1'] = tags[34].getText()
+        sheet['U1'] = tags[40].getText()
+        sheet['X1'] = tags[46].getText()
     except IndexError:
         return None
     cell = str(row_no)
@@ -32,36 +30,65 @@ def get_vtu_page(sheet, url, usn, row_no):
     sheet['A'+cell] = usn[3:]
     sheet['B'+cell] = name[1:]
     col = 'C'
-    for i in range(8, n-6, 6):
-        sheet[col+cell] = tags[i].getText()
-        col = get_next_cell(col)
-        if col == 'K':
+    for i in range(6, n, 6):
+        for j in range(3):
+            sheet[col+cell] = tags[i+j].getText()
+            col = get_next_cell(col)
+        if col == "AA":
             break
+        # sheet[col+cell] = tags[i+1].getText()
 
 
 def get_next_cell(current_cell):
+    if current_cell == 'Z':
+        return "AA"
     return chr(ord(current_cell)+1)
 
 
-url = "http://results.vtu.ac.in/cbcs_17/result_page.php"
-start_seq = "1BI15CS"
-pwd = os.getcwd()
-path = os.path.join(pwd, 'tests')
-if not os.path.exists(path):
-    os.makedirs(path)
+def crawl(url, start_seq, sheet, title, filter_sub):
+    sheet.title = title
+    sheet.merge_cells('C1:E1')
+    sheet.merge_cells('F1:H1')
+    sheet.merge_cells('I1:K1')
+    sheet.merge_cells('L1:N1')
+    sheet.merge_cells('O1:Q1')
+    sheet.merge_cells('R1:T1')
+    sheet.merge_cells('U1:W1')
+    sheet.merge_cells('X1:Z1')
+    sheet['A1'] = 'USN'
+    sheet['B1'] = 'Name'
+    init_cols = 'B'
+    cell_value = 2
+    for i in range(0, 8):
+        init_cols = get_next_cell(init_cols)
+        cell = init_cols + str(cell_value)
+        sheet[cell] = "IA"
+        init_cols = get_next_cell(init_cols)
+        cell = init_cols + str(cell_value)
+        sheet[cell] = "EA"
+        init_cols = get_next_cell(init_cols)
+        cell = init_cols + str(cell_value)
+        sheet[cell] = "Total"
+    for i in range(1, 500):
+        print('.', end='', flush=True)
+        if len(str(i)) == 1:
+            roll = "00" + str(i)
+        elif len(str(i)) == 2:
+            roll = "0" + str(i)
+        else:
+            roll = str(i)
+        usn = start_seq + roll
+        get_vtu_page(sheet, url, usn, i + 2, filter_sub)
+
+
 wb = openpyxl.Workbook()
-sheet = wb.get_active_sheet()
-sheet.title = "5th Sem"
+sheet1 = wb.get_active_sheet()
 print("working", end='', flush=True)
-for i in range(1, 200):
-    print('.', end='', flush=True)
-    if len(str(i)) == 1:
-        roll = "00"+str(i)
-    elif len(str(i)) == 2:
-        roll = "0"+str(i)
-    else:
-        roll = str(i)
-    usn = start_seq + roll
-    get_vtu_page(sheet, url, usn, i+1)
+crawl("http://results.vtu.ac.in/cbcs_17/result_page.php", "1BI16CS", sheet1, "2nd sem", "15MAT21")
+print("\n")
+sheet1 = wb.create_sheet(index=1)
+crawl("http://results.vtu.ac.in/cbcs_17/result_page.php", "1BI15CS", sheet1, "4th sem", "15MAT41")
+sheet1 = wb.create_sheet(index=2)
+crawl("http://results.vtu.ac.in/results17/index.php", "1BI14CS", sheet1, "6th sem", "10AL61")
 print("\ndone.")
 wb.save("output.xlsx")
